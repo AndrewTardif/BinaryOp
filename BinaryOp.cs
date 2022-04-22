c528546154118d0869a77394f2f1c06895e77756
epicsaxgandalf <43189142+epicsaxgandalf@users.noreply.github.com>
2022-04-21 19:22:47 -0700

    Add Finished

--- BinaryOp.cs
+++ BinaryOp.cs
@@ -9,7 +9,11 @@ namespace BinaryOp
 
     class Add<T> : BinaryOperator<T>
     {
-
+        public T call(T a, T b) {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            return (T)(a1 + b1);
+        }
     }
 
     class Subtract<T> : BinaryOperator<T> 
